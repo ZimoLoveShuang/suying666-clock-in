371271f13827bb2f698c136d763a70665bf00222
zimo <461009747@qq.com>
2021-04-21 12:44:06 +0800

    修复速鹰666切换域名之后不能自动打卡领流量的bug

--- index.py
+++ index.py
@@ -8,20 +8,21 @@ email = ''
 passwd = ''
 # session
 session = requests.session()
+host = 'suying999.net'
 
 
 # 登陆
 def login():
-    url = 'http://www.tunxing.com/auth/login'
+    url = 'https://{host}/auth/login'.format(host=host)
     headers = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9',
         'Connection': 'keep-alive',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Host': 'www.tunxing.com',
-        'Origin': 'http://www.tunxing.com',
-        'Referer': 'http://www.tunxing.com/auth/login',
+        'Host': host,
+        'Origin': 'http://' + host,
+        'Referer': 'https://{host}/auth/login'.format(host=host),
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
         'X-Requested-With': 'XMLHttpRequest',
     }
@@ -38,26 +39,28 @@ def login():
 
 # 速鹰666签到领流量
 def clockIn():
-    url = 'http://www.tunxing.com/user/checkin'
+    url = 'https://{host}/user/checkin'.format(host=host)
     headers = {
-        'Host': 'www.tunxing.com',
+        'Host': host,
         'Connection': 'keep-alive',
         'Content-Length': '0',
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'X-Requested-With': 'XMLHttpRequest',
-        'Origin': 'http://www.tunxing.com',
-        'Referer': 'http://www.tunxing.com/user',
+        'Origin': 'http://' + host,
+        'Referer': 'https://{host}/user'.format(host=host),
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9',
     }
     res = session.post(url=url, headers=headers)
     json = res.json()
+    print(json)
     return json
 
 
 def sendMessage(msg):
     url = 'https://sc.ftqq.com/' + key + '.send?text=' + msg;
-    requests.get(url)
+    res = requests.get(url)
+    print(res.json())
 
 
 def main_handler(event, context):

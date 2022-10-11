c86ab110671343b3343aa356952aa3f23dfb6d9f
子墨 <461009747@qq.com>
2022-10-11 19:20:21 +0800

    删除无用文件 新增自动更新suying的host

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
+import re
+
 import requests
+from bs4 import BeautifulSoup
 
 # server酱key
 key = ''
@@ -8,46 +11,56 @@ email = ''
 passwd = ''
 # session
 session = requests.session()
-host = 'suying999.net'
 
 
 # 登陆
-def login():
-    url = 'https://{host}/auth/login'.format(host=host)
-    headers = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Host': host,
-        'Origin': 'http://' + host,
-        'Referer': 'https://{host}/auth/login'.format(host=host),
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-        'X-Requested-With': 'XMLHttpRequest',
-    }
+def login(host):
+    url = '{}/auth/login'.format(host)
     params = {
         'email': email,
         'passwd': passwd,
         'code': ''
     }
-
-    res = session.post(url=url, headers=headers, data=params)
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': url,
+        'Referer': '{}/auth/login'.format(url),
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+        'X-Requested-With': 'XMLHttpRequest',
+    }
+    res = session.post(url=url, headers=headers, data=params, timeout=10)
     msg = res.json()['msg']
+    print(msg)
+    if msg == '登录成功':
+        # 登陆成功之后，去更新hosts.txt
+        del headers['Content-Type']
+        html = session.get('{}/user'.format(host), headers=headers).text
+        soup = BeautifulSoup(html, 'lxml')
+        # for i in soup.find_all(text=re.compile('<h5 id=".*"><a class="reference-link" .*</a></h5>')):
+        hosts = set()
+        for i in soup.find_all('h5'):
+            a = i.find('a', text=re.compile('http.*'))
+            if a:
+                hosts.add(a.text)
+        with open('hosts.txt', 'w', encoding='utf-8') as f:
+            for i in hosts:
+                f.write('{}\n'.format(i))
     return msg
 
 
 # 速鹰666签到领流量
-def clockIn():
-    url = 'https://{host}/user/checkin'.format(host=host)
+def clockIn(host):
+    url = '{}/user/checkin'.format(host)
     headers = {
-        'Host': host,
         'Connection': 'keep-alive',
         'Content-Length': '0',
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'X-Requested-With': 'XMLHttpRequest',
-        'Origin': 'http://' + host,
-        'Referer': 'https://{host}/user'.format(host=host),
+        'Origin': url,
+        'Referer': '{}/user'.format(host),
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9',
     }
@@ -64,12 +77,22 @@ def sendMessage(msg):
 
 
 def main_handler(event, context):
-    msg = login()
-    print(msg)
-    json = clockIn()
-    msg = json['msg']
-    print(msg)
-    sendMessage(msg)
+    hosts = [i.strip() for i in open('hosts.txt', 'r', encoding='utf-8').readlines()]
+    for host in hosts:
+        try:
+            print('try', host)
+            msg = login(host)
+            json = clockIn(host)
+            msg = json['msg']
+            ret = json['ret']
+            print(ret)
+            print(msg)
+            if ret == 0:
+                if msg != '您似乎已经签到过了...':
+                    sendMessage(msg)
+                exit()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
